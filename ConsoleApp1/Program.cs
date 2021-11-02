9cfa0944b9d71628a88011ecf81dfd38abf84a8e
daehwan <eoghks960307@naver.com>
2021-11-02 17:08:22 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자를 입력하세요:");
+
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
