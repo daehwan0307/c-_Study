969802d14e8b1d1546dba7f3f8fb600670ec7817
daehwan <eoghks960307@naver.com>
2021-11-24 15:43:56 +0900

    서버 및 ui

---
+++ ConsoleApp1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ConsoleApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbDataServer = new System.Windows.Forms.GroupBox();
+            this.tbDataServer = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.gbDataServer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbDataServer
+            // 
+            this.gbDataServer.Controls.Add(this.tbDataServer);
+            this.gbDataServer.Controls.Add(this.label5);
+            this.gbDataServer.ForeColor = System.Drawing.Color.Black;
+            this.gbDataServer.Location = new System.Drawing.Point(35, 123);
+            this.gbDataServer.Name = "gbDataServer";
+            this.gbDataServer.Size = new System.Drawing.Size(643, 54);
+            this.gbDataServer.TabIndex = 2;
+            this.gbDataServer.TabStop = false;
+            this.gbDataServer.Text = " Data Server ";
+            this.gbDataServer.Enter += new System.EventHandler(this.gbDataServer_Enter);
+            // 
+            // tbDataServer
+            // 
+            this.tbDataServer.Location = new System.Drawing.Point(153, 19);
+            this.tbDataServer.Margin = new System.Windows.Forms.Padding(4);
+            this.tbDataServer.Name = "tbDataServer";
+            this.tbDataServer.Size = new System.Drawing.Size(408, 21);
+            this.tbDataServer.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(82, 23);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 12);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Domain :  ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 491);
+            this.Controls.Add(this.gbDataServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbDataServer.ResumeLayout(false);
+            this.gbDataServer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbDataServer;
+        private System.Windows.Forms.TextBox tbDataServer;
+        private System.Windows.Forms.Label label5;
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,74 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.IO;
+using Newtonsoft.Json.Linq;
 
-namespace ConsoleApp1
+class Test
 {
-    class Program
+    static void Main(string[] args)
     {
-        //*프로그램 실행이 시작되는 곳
-        static void Main(string[] args)
-        {
-            /* Console.WriteLine("숫자를 입력하세요:");
 
-             string input = Console.ReadLine();
-
-             int number = Int32.Parse(input);
-
-             if (number < 0)
-                 Console.WriteLine("음수이며,");
-             else if (number > 0)
-                 Console.WriteLine("양수이며,");
-             else
-                 Console.WriteLine("0");
-
-             if (number % 2 == 0)
-                 Console.WriteLine("짝수이다.");
-             else
-                 Console.WriteLine("홀수이다.");
-                 */
-
-            string folderpath = "C:/Users/daehwan/Desktop/lsm개새";
-
-            DirectoryInfo di = new DirectoryInfo(folderpath);
-
-            if (di.Exists)
-            {
-                Console.WriteLine("폴더가 이미 존재합니다.");
-
-            }
-            else
-            {
-                di.Create();
-               
-            }
-
-            string filepath = "C:/Users/daehwan/Desktop/lsm개새/정경창개새.txt";
-            Console.WriteLine("입력할 내용 :");
-
-            string textValue = Console.ReadLine();
-            System.IO.File.WriteAllText(filepath, textValue, Encoding.Default);
-
-
-            StreamWriter ndi = new StreamWriter("C:/Users/daehwan/Desktop/lsm개새/정경창개새.txt");
-            ndi.Write("오홍석_개새");
-            ndi.Close();
-
-            string con = File.ReadAllText(@"C:/Users/daehwan/Desktop/lsm개새/정경창개새.txt");
-            Console.Write("변경후:");
-            Console.WriteLine(con);
-
-
-
-            string[] lsmtask = con.Split('_');
-            Console.WriteLine(lsmtask[0]);
-            Console.WriteLine(lsmtask[1]);
-
-           
-        }
+        Console.WriteLine("dk");
+        bringpnum();
+       
+      
     }
-   
+
+    static string bringpnum()
+    {
+        Console.Write("url입력해주세요:");
+        string EmrServerUrl =   Console.In.ReadLine();
+        WebRequest request = WebRequest.Create(EmrServerUrl); // 호출할 url =  https://apiv1.mevitz.com/test/getPhoneNumber
+
+        request.Method = "Post";
+        WebResponse response = request.GetResponse();
+        Stream dataStream = response.GetResponseStream();
+        StreamReader reader = new StreamReader(dataStream);
+
+        string responseFromServer = reader.ReadToEnd();
+
+        JObject obj = JObject.Parse(responseFromServer);
+        string pnum = (string)obj["patient_phone"];
+       
+
+
+        reader.Close();
+        dataStream.Close();
+        response.Close();
+
+        Console.WriteLine(pnum);
+        return pnum;
+    }
 }
